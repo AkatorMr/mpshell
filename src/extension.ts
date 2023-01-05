0bc64889c4b2bce0faa86534a1a4d02c64ee5b42
AkatorMr <marck_sparkle@hotmail.com>
2023-01-04 22:59:35 -0300

    Add pack

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SerialPort } from 'serialport';
+import { autoDetect } from '@serialport/bindings-cpp'
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,11 +20,24 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from mpShell!');
-		
+
+	});
+
+	let syncdata = vscode.commands.registerCommand('mpshell.syncdata', async () => {
+
+		const ports = await SerialPort.list();
+
+		console.log(ports);
+		/* 
+		SerialPort.list().then((value: PortInfo[]): void => {
+
+		}).catch((reason: any) => { }); */
+
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(syncdata);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
